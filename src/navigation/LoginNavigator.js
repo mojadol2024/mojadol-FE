0354260a353c4c80beb931f1bd951fe10e3041d8
변경태 <byeongyeongtae@byeongyeongtaeui-MacBookPro.local>
2024-11-26 18:18:09 +0900

    refactor

--- src/navigation/LoginNavigator.js
+++ src/navigation/LoginNavigator.js
@@ -9,12 +9,18 @@ import DogRegistration from '../screens/DogRegistration';
 import PaymentScreen from '../screens/Payment';
 import MyPage from '../screens/MyPage';
 import MissingDogRegistration from '../screens/MissingDogRegistration';
+import SplashScreen from '../screens/SplashScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+import StartLogin from '../screens/StartLogin';
 const Stack = createStackNavigator();
 
 const LoginNavigator = () => {
   return (
     <Stack.Navigator>
+      <Stack.Screen name="SplashScreen" component={SplashScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+      <Stack.Screen name="StartLogin" component={StartLogin} />
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Board" component={Board} />
       <Stack.Screen name="BoardDetail" component={BoardDetail} />
